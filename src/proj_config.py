7dfe961cb6722ba6e19b7ccf092dc283467bc1c7
chgwan <chenguang.wan@outlook.com>
2026-06-12 17:33:11 +0800

    refactor: remove reduntant paths

--- src/proj_config.py
+++ src/proj_config.py
@@ -11,21 +11,6 @@ class ProjConfig(BaseSettings):
     proj_db_dir: pathlib.Path = base_dir.joinpath('ProjDB')
     DATABASE_dir: pathlib.Path = strpath2path(base_config['data']['database_dir'])
 
-    # --- WEST boundary data subdirs (derived from DATABASE_dir) ---
-    dcs_org_dir: ClassVar[pathlib.Path] = DATABASE_dir / 'DCSOrg'
-    dcs_h5_dir: ClassVar[pathlib.Path] = DATABASE_dir / 'DCSH5'
-    data_org_dir: ClassVar[pathlib.Path] = DATABASE_dir / 'DataOrg'
-    merged_dir: ClassVar[pathlib.Path] = DATABASE_dir / 'Merged'
-
-    # --- ProjDB output layout (derived from proj_db_dir) ---
-    stats_dir: ClassVar[pathlib.Path] = proj_db_dir / 'Stats'
-    figures_dir: ClassVar[pathlib.Path] = proj_db_dir / 'Figures'
-    shot_status_csv: ClassVar[pathlib.Path] = stats_dir / 'shot_status.csv'
-    shot_freq_stats_csv: ClassVar[pathlib.Path] = stats_dir / 'shot_freq_stats.csv'
-    lcfs_fig_dir: ClassVar[pathlib.Path] = stats_dir / 'LCFS'
-    sig_freqs_fig_dir: ClassVar[pathlib.Path] = stats_dir / 'SigFreqs'
-    dcs_actual_vs_ref_dir: ClassVar[pathlib.Path] = figures_dir / 'dcs_actual_vs_ref'
-
     # --- DCS scope column semantics: (label, matrix-column-index) ---
     dcs_columns: ClassVar[tuple] = (('ref', 0), ('actual', 3))
     # h5 key of the reconstructed-boundary target signal
